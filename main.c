0fa6383558e4c5d819354a4141ca86846b16fd10
Geremia <Geremia@users.noreply.github.com>
2017-01-03 20:16:17 -0700

    print stats. AMDG.

--- main.c
+++ main.c
@@ -13,14 +13,14 @@ typedef struct {
 
 typedef struct {
     vec pos, vel, acc;
+    //mass assumed = 1
+    //speed of light c also assumed = 1, for simplicity's sake
 } obj;
 
 //Generate a vector in a random direction, of random length ≤ L
 vec randVec(double L)
 {
     double x = L, y = L, z = L;
-
-    //srandom(23483920); //Seed the RNG.
     while (sqrt(x * x + y * y + z * z) > L) {
 	x = rand11() * L;
 	y = rand11() * L;
@@ -73,18 +73,41 @@ obj* generate(int numObjects, double rad)
     return objects;
 }
 
-void printOut(obj *objects, int numObjects) {
+//Compute the norm (magnitude) of a vector.
+double vecNorm(vec *v) {
+	return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+}
+
+void printPhysicsStats(obj *objects, int numObjects, FILE *outFile) {
+	vec CoM = {0}; //center of mass position vector
+	double totalVel = 0, avgVel = 0, totalAcc = 0, avgAcc = 0;
+	for (int i = 0; i < numObjects; i++) {
+		totalVel += vecNorm(&objects[i].vel);
+		totalAcc += vecNorm(&objects[i].acc);
+	}
+	avgVel = totalVel / (double)numObjects;
+	avgVel = totalVel / (double)numObjects;
+	fprintf(outFile, "# Total Velocity: % e  Average Velocity: % e   "
+			 "Total Acceleration: % e  Average Acceleration: % e\n",
+			 totalVel, avgVel, totalAcc, avgAcc);
+}
+
+void printOut(obj *objects, int numObjects, FILE *outFile) {
 	vec pos, vel, acc;
-	printf("%3s  %13s %13s %13s  %13s %13s %13s  %13s %13s %13s\n",
-			"#ID",
-			"x", "y", "z",
-			"vx", "vy", "vz",
-			"ax", "ay", "az");
+	//print physics stats (total KE, RMS vel./acc., etc.) for time-snapshot
+	printPhysicsStats(objects, numObjects, outFile);
+	//print header
+	fprintf(outFile, "%3s  %13s %13s %13s  %13s %13s %13s  %13s %13s %13s\n",
+			 "#ID",
+			 "x", "y", "z",
+			 "vx", "vy", "vz",
+			 "ax", "ay", "az");
+	//print object data
 	for (int i = 0; i < numObjects; i++) {
 		pos = objects[i].pos;
 		vel = objects[i].vel;
 		acc = objects[i].acc;
-		printf("%3d  % e % e % e  % e % e % e  % e % e % e\n",
+		fprintf(outFile, "%3d  % e % e % e  % e % e % e  % e % e % e\n",
 				i, //id of object
 				pos.x, pos.y, pos.z,
 				vel.x, vel.y, vel.z,
@@ -93,8 +116,17 @@ void printOut(obj *objects, int numObjects) {
 }
 
 int main() {
-	int n = 1E2;
-	obj *objects = generate(n, 1.0);
-	printOut(objects, n);
+	int n = 8; //number of objects
+	double radius = 128; //radius of "Plummer" sphere
+
+    	srandom(23483920); //Seed the RNG.
+
+	obj *objects = generate(n, radius);
+
+	double t_0, t_f, dt;
+
+
+	printOut(objects, n, stdout);
+
 	return 0;
 }
