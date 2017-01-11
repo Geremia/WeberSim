e551b7c153aba0c3085d176ba9f21150a2fbd494
Geremia <Geremia@users.noreply.github.com>
2017-01-10 17:51:18 -0700

    Python script works, Deo gratias!

--- blender_script.py
+++ blender_script.py
@@ -16,68 +16,37 @@ def fileNameToData(filepath):
                                                 #(ignore data files header info)
      data=[]
      for i in lines:
-         data.append(list(map(float,i.split())))
+         data.append(list(map(float,i.split()[1:4])))
      return data
 
-#number of objects simulated
+#number of objects / vertices
 n = len(fileNameToData("000000.txt")) 
 
 #Read in all data files.
 #(ibid. pp. 8-6 – 8-7)
 files = glob.glob('*.txt')
 files.sort()
-data=[]
+data = [] #data is of the format ((frame 1 verts: (2,3,3), (3,6,2), …), (frame 2 verts: (0,5,2), (3,3,5), …), …)
 for f in files:
-    for i in fileNameToData(f):
-        data.append(i)
+    data.append(fileNameToData(f))
 
-#add circle:
-bpy.ops.object.mode_set(mode='OBJECT')
-bpy.ops.mesh.primitive_circle_add()
+n_frames = len(files)
+bpy.context.scene.frame_end = n_frames
 
-bpy.ops.object.mode_set(mode='EDIT')
-obj = bpy.data.objects['Circle']
-bm = bmesh.from_edit_mesh(obj.data)
+#add mesh object:
+bpy.ops.mesh.primitive_circle_add(vertices=n)
+obj = bpy.context.active_object
 
-#delete all vertices
-for v in bm.verts:
-    bm.verts.remove(v)
-
-#add back correct number vertices
-for i in range(n):
-    bm.verts.new((0,0,0))
-
-#Switch to object mode
-bpy.ops.object.mode_set(mode = 'OBJECT')
-#Create initial Basis Shape Key
-bpy.ops.object.shape_key_add()
-#Create initial Shape Key
-bpy.ops.object.mode_set(mode = 'OBJECT')
-bpy.ops.object.shape_key_add()
-bpy.data.shape_keys[0].key_blocks["Key 1"].name = 'Key 1'
-bpy.data.shape_keys[0].key_blocks["Key 1"].value = 1
-bpy.data.shape_keys[0].key_blocks["Key 1"].keyframe_insert("value", frame=0)
-
-#add in keyframes
-#Key a single frame per data file
-frames = len(files)
-framecount = 0
-rowcount = 0
-obj = bpy.data.objects['Circle']
-bpy.ops.object.mode_set(mode='EDIT')
-mesh = obj.data
-bm = bmesh.from_edit_mesh(obj.data)
-for j in range(1, frames):
-    for keyname in bpy.data.shape_keys[0].key_blocks.keys():
-        bpy.data.shape_keys[0].key_blocks[keyname].value = 0
-        bpy.data.shape_keys[0].key_blocks[keyname].keyframe_insert("value", frame = framecount)
-        for i in range(0,len(bm.verts)-1):
-            try:
-                bm.verts[i].co.x = data[rowcount][1]
-                bm.verts[i].co.y = data[rowcount][2]
-                bm.verts[i].co.z = data[rowcount][3]
-                rowcount += 1
-                bmesh.update_edit_mesh(obj.data)
-            except:
-                pass
-    framecount += 1
+for i_frame in range(n_frames):
+    block = obj.shape_key_add(name=str(i_frame), from_mix=False)  # returns a key_blocks member
+    block.value = 1.0
+    block.mute = True
+    vertex_idx = 0
+    for (vert, co) in zip(block.data, data[i_frame]):
+        vert.co = co
+    block.mute = True
+    block.keyframe_insert(data_path='mute', frame=0, index=-1)
+    block.mute = False
+    block.keyframe_insert(data_path='mute', frame=i_frame + 1, index=-1)
+    block.mute = True
+    block.keyframe_insert(data_path='mute', frame=i_frame + 2, index=-1)
